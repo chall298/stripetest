466725c8ecc1697d100214b841fdf5b78286dbf5
Christopher Hall <chall361@gmail.com>
2020-01-29 22:14:09 +0000

    stripe test

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,19 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+import { render } from "react-dom";
+import { StripeProvider } from "react-stripe-elements";
+
+import MyStoreCheckout from "./MyStoreCheckout";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const App = () => {
+	return (
+		<StripeProvider apiKey="pk_test_12345">
+			<MyStoreCheckout />
+		</StripeProvider>
+	);
+};
+
+render(<App />, document.getElementById("root"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
